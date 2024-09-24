1ba90da8a7c23acb0b35c4633d11b1d664f7a1c4
Deepika Vennapusa <166745635+deepu-434@users.noreply.github.com>
2024-09-24 19:18:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+//import LikeRating from "./components/LikeRating";
+//function App(){
+ // return(
+   // <div>
+     // <LikeRating/>
+      //<LikeRating/>
+       //<LikeRating/>
+
+
+       
+  //   </div>
+ // );
+//}
+//export default App;
+import Calculator from "./components/calculator/calculator";
+function App(){
+    return(
+        <>
+          <Calculator/>
+        </>
+    );
+}
+export default App;
